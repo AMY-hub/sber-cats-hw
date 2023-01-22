e5c62d57f9fa06d4db9179b5fc6f8008a35c7199
AMY-hub <ianastasiai65@gmail.com>
2023-01-22 17:43:52 +0300

    Updates

---
+++ js/index.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/index.js":
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/***/ (function() {
+
+eval("var cards = document.getElementsByClassName(\"card\");\nfor (var i = 0, cnt = cards.length; i < cnt; i++) {\n  var width = cards[i].offsetWidth;\n  cards[i].style.height = width * 0.6 + \"px\";\n}\n\n//# sourceURL=webpack://webpack/./src/scripts/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scripts/index.js"]();
+/******/ 	
+/******/ })()
+;
